2634aa36dea29fccc5ea693a83c2f092b66d8df1
Nima Gardideh <nima@halfmoon.ws>
2018-04-04 10:05:37 -0400

    Updated version.

--- adWordsObject.js
+++ adWordsObject.js
@@ -36,7 +36,7 @@ function AdWordsObject(options) {
   self.credentials = null;
   self.tokenUrl = 'https://www.googleapis.com/oauth2/v3/token';
   self.verbose = self.options.verbose;
-  self.version = self.options.version || 'v201705';
+  self.version = self.options.version || 'v201802';
 
   self.refresh = function(done) {
     // check if current credentials haven't expired
